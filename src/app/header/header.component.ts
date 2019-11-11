338004a99fd8633b9ead964ee63cb09a3aaf86e7
jm_b <jmbanchero@gmail.coom>
2019-11-11 18:47:10 +0000

    add

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild, EventEmitter, Output } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { SearchInputComponent } from './search-input/search-input.component';
+import { SearchResult } from '../Models/SearchResult';
 
 
 
@@ -13,12 +14,20 @@ export class HeaderComponent implements OnInit {
   @ViewChild('navbar', { static: true }) navbar: HeaderComponent;
   @Output() onSearch: EventEmitter<null> = new EventEmitter<null>();
 
+  results: SearchResult[];
+  loading: boolean;
+
+
   constructor(private route: ActivatedRoute,
     private router: Router) { }
 
   ngOnInit() {
   }
 
+  updateResults(results: SearchResult[]): void {
+    this.results = results;
+    // console.log("results:", this.results); // uncomment to take a look
+  }
   goToRoute(route: string) {
     console.log(' ---> vamos pata la ruta', route);
     this.router.navigate([`/${route}`]);
